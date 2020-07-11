b30851c1bd1db516e8a4944ffeaabf60ff1725c4
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-07-10 21:06:14 -0400

    [Re_Working_Code]

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 5190 ;
 var http = require('http');
 var   fs = require('fs'  );
 
-const HN1_Get_Fas =function( src_pat ){
+const HN2_Get_Fas =function( src_pat ){
 
     var pro=( new Promise() );
 
@@ -20,9 +20,18 @@ const HN1_Get_Fas =function( src_pat ){
     return( pro );
 };;
 
-const HN1_Ser_Fil =function( rar,pac ){ "use strict"
+const HN2_SQL_Get_Tes( rar_dap ){
+
+    rar[1].end("[TODO:HN2_SQL_Get_Tes]");
+
+};;
+
+const HN2_Ser_Fil =function( rar_dap ){ "use strict"
+
+    var rar=rar_dap[ 0 ];
+    var dap=rar_dap[ 1 ];
     
-    fs.readFile( pac[0],function(obj_err,dat_fil){
+    fs.readFile( dap[0],function(obj_err,dat_fil){
 
         if(obj_err){
 
@@ -30,7 +39,7 @@ const HN1_Ser_Fil =function( rar,pac ){ "use strict"
 
         }else{
 
-            rar[1].writeHead(200,{ "Content-Type": pac[1] });
+            rar[1].writeHead(200,{ "Content-Type": dap[1] });
             rar[1].end( dat_fil , "utf-8" );
 
         };;
@@ -38,23 +47,75 @@ const HN1_Ser_Fil =function( rar,pac ){ "use strict"
 
 };;
 
-const HN1_Mai=function(){ "use strict"
+const HN2_Rou=function( req , res ){ "use strict"
 
-    http.createServer( function(req, res){
-        var rar=[ req,res ];
+    //:Declare_And_Summarize_All_Function_Variables:
+    var rar     =[ req,res ]; //:Request_And_Response_Tuple
+    var url     = rar[0].url; //:Requested_URL
+    var tab_daw = null      ; //:TABle_of:Data_and_Whatever
+    var     daw = null      ; //:selected:Data_and_Whatever
+    var tab_act = null      ; //:TABle_of:ACTion(s)
+    var     act = null      ; //:selected:ACTion
+    var rar_daw = null      ; //:[ rar, daw ]
+    
+    //:To_Avoid_Clutter_Summarize_And_Label_Actions_Below:
+    //: 01: [ shortcut / path ]==>[ data , action ]
+    //:     In other words:
+    //:         tab_daw[ url ][ 0 ] == Associated data
+    //:         tab_daw[ url ][ 1 ] == What to do with data?
+    //:
+    //: 02: Decide what [ data, action ] pair to use:
+    //:     In other words:
+    //:         what_to_do_with_data_FUNCTION=(
+    //:             tab_act[ what_to_do_with_data_STRING ] )
+    //:         
+    //: 03: [ action (string) ]==>[ action (function) ]
+    //: 04: Select action to use with data:
+    //: 05: Help connect_the_dots when reading code.
+    //: 06: Perform selected action on data:
+             
+    /* 01 */    tab_daw={  
+    /* -- */        "/K" : [ "./key._"   , "text/plain"      ]
+    /* -- */    ,   "/H" : [ "./htm._"   , "text/html"       ]
+    /* -- */    ,   "/J" : [ "./j_s._"   , "text/javascript" ]
+    /* -- */    ,   "/T" : [ "./sql._"   , "SQL_GET_TEST"    ]
+    /* -- */    };;
+    /* 02 */    daw=( tab_daw[ url ] || tab_daw[ "/K" ] );
+    /* 03 */    tab_act={ 
+    /* -- */        "text/plain"      : HN2_Ser_Fil
+    /* -- */    ,   "text/html"       : HN2_Ser_Fil
+    /* -- */    ,   "text/javascript" : HN2_Ser_Fil
+    /* -- */    ,   "SQL_GET_TEST"    : HN2_SQL_Get_Tes
+    /* -- */    };;
+    /* 04 */    act = tab_act[ daw[ 1 ] ];
+    /* 05 */    rar_daw=[rar,daw]; 
+    /* 06 */    act(     rar_daw );
 
-        var jum_dic={
-            "/K" : [ "./key._" , "text/plain"      ]
-        ,   "/H" : [ "./htm._" , "text/html"       ]
-        ,   "/J" : [ "./j_s._" , "text/javascript" ]
-        };;
-
-        HN1_Ser_Fil( rar 
-        ,   jum_dic[ rar[0].url ] //:Selected_Path
-        ||  jum_dic[    "/K"    ] //:Default__Path
-        );;
-
-    }).listen(PORT);
 };;
 
-HN1_Mai();
+const HN2_Mai=function(){ "use strict"
+
+    http.createServer( HN2_Rou ).listen(PORT);
+
+};;
+HN2_Mai();
+
+/** 
+    HN2_Rou: Main request routing function.
+    HN2_Mai: Main entry point for project HN2 (HerokuNodeLean02)
+    ent[1]: Action to perform on data
+    ent[0]: The data associated with selected route
+
+        daw:Data_And_Whatever (Usually:What to do with the data)
+    tab_daw:TABle_of_Data_And_Whatever
+    tab_rou:TABle_of_ROUtes (SEE: tab_dap)
+    tab_dap:TABle_of_DataActionPairs ( [data,action] )
+    tab_dap:Old, See: tab_daw
+    tab_act:TABle_of_ACTions
+    sel_act:Selected_Action
+        act:Shorthand for sel_act
+
+    dic: An object used as a dictionary.
+    ent: Entry taken from dictionary or array.
+    url: String path requested by client.
+**/
