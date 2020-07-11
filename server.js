2190c6501a71fb74222cb866ae6b5f1c83e7b1ef
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-07-10 22:06:35 -0400

    [promise]

--- server.js
+++ server.js
@@ -21,12 +21,13 @@ const HN2_Get_Fas =function( src_pat ){
     fs.readFile( src_pat,function(obj_err,dat_fil ){
         if( obj_err ){
 
-            //throw( obj_err );
-            pro.reject( obj_err );
+            //:reject
+            pro.catch( obj_err );
 
         }else{
 
-            pro.accept( dat_fil );
+            //:accept
+            pro.then( dat_fil );
 
         };;
     });;
